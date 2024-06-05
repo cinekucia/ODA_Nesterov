41e77cddbf25be14a561e797cf6d337803bfc280
Bartosz Grabek <bartoszgrabek7@gmail.com>
2024-06-05 12:00:45 +0200

    doc: added docstring and typehints to common

--- optim/common.py
+++ optim/common.py
@@ -1,9 +1,19 @@
 import numpy as np
 
 
-def lasso_objective(X, y, beta, lambda_):
-    """
-    Compute the LASSO objective function.
+def lasso_objective(
+    X: np.ndarray, y: np.ndarray, beta: np.ndarray, lambda_: float
+) -> float:
+    """Computes the objective function for LASSO regression.
+
+    Args:
+        X (array_like): The feature matrix of shape (n_samples, n_features).
+        y (array_like): The target values of shape (n_samples,).
+        beta (array_like): The coefficient vector of shape (n_features,).
+        lambda_ (float): The regularization parameter.
+
+    Returns:
+        float: The value of the objective function for LASSO regression.
     """
     n = len(y)
     residual = y - np.dot(X, beta)
@@ -12,9 +22,20 @@ def lasso_objective(X, y, beta, lambda_):
     return loss + penalty
 
 
-def lasso_gradient(X, y, beta, lambda_):
+def lasso_gradient(
+    X: np.ndarray, y: np.ndarray, beta: np.ndarray, lambda_: float
+) -> np.ndarray:
     """
     Compute the gradient of the LASSO objective function.
+
+    Args:
+        X (ndarray): The feature matrix of shape (n_samples, n_features).
+        y (ndarray): The target values of shape (n_samples,).
+        beta (ndarray): The coefficient vector of shape (n_features,).
+        lambda_ (float): The regularization parameter.
+
+    Returns:
+        ndarray: The gradient of the LASSO objective function with respect to the coefficient vector.
     """
     n = len(y)
     residual = y - np.dot(X, beta)
