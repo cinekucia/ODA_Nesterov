44a6e3fddd89ec10983249be6494b07f545e40e8
Bartosz Grabek <bartoszgrabek7@gmail.com>
2024-06-05 09:12:21 +0200

    ref: moved repeated functions to common

--- optim/dual.py
+++ optim/dual.py
@@ -1,20 +1,5 @@
 import numpy as np
-
-
-def lasso_objective(X, y, beta, lambda_):
-    n = len(y)
-    residual = y - np.dot(X, beta)
-    loss = 0.5 * np.dot(residual, residual) / n
-    penalty = lambda_ * np.linalg.norm(beta, 1)
-    return loss + penalty
-
-
-def lasso_gradient(X, y, beta, lambda_):
-    n = len(y)
-    residual = y - np.dot(X, beta)
-    gradient_loss = -np.dot(X.T, residual) / n
-    gradient_penalty = lambda_ * np.sign(beta)
-    return gradient_loss + gradient_penalty
+from .common import lasso_objective, lasso_gradient
 
 
 def dual_gradient(
