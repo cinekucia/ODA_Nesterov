926ead6023f46387e2f6e330542fb4284542b501
Bartosz Grabek <bartoszgrabek7@gmail.com>
2024-06-10 22:16:09 +0200

    feat: add new generating function to imports util

--- util/__init__.py
+++ util/__init__.py
@@ -1,2 +1,2 @@
-from .synthetic import generate_sparse_least_squares, generate_sparse_least_squares_2
+from .synthetic import generate_sparse_least_squares, generate_sparse_least_squares_2, generate_test_data
 from .datasets import load_diabetes_data
