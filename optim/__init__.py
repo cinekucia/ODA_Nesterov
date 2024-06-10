60e2feaf058731afb174dd44e9483f97c77cc654
Bartosz Grabek <bartoszgrabek7@gmail.com>
2024-06-10 22:15:26 +0200

    fix: add new imports to package optim

--- optim/__init__.py
+++ optim/__init__.py
@@ -1,4 +1,4 @@
 from .nesterov import nesterov_accelerated_gradient
-from .primal import primal_gradient
-from .dual import dual_gradient
+from .primal import primal_gradient, primal_gradient_new
+from .dual import dual_gradient, dual_gradient_new
 from .common import lasso_objective, lasso_gradient
